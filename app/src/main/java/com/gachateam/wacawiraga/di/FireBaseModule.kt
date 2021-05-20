cac1f928698668e6251df66f70ae64154f7caaf5
ad <da>
2021-05-20 14:50:29 +0700

    add library for remote-download model ML

---
+++ app/src/main/java/com/gachateam/wacawiraga/di/FireBaseModule.kt
@@ -0,0 +1,26 @@
+package com.gachateam.wacawiraga.di
+
+import com.google.mlkit.common.model.CustomRemoteModel
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.common.model.RemoteModelManager
+import com.google.mlkit.linkfirebase.FirebaseModelSource
+
+object FireBaseModule {
+    private const val CUSTOM_MODEL_NAME = "model name"
+
+    // Specify the name you assigned in the Firebase console.
+    val remoteModel =
+        CustomRemoteModel
+            .Builder(FirebaseModelSource.Builder(CUSTOM_MODEL_NAME).build())
+            .build()
+
+    val downloadConditions = DownloadConditions.Builder()
+        .build()
+
+    //download model if not exist or update
+//     RemoteModelManager.getInstance().download(remoteModel, downloadConditions)
+//        .addOnSuccessListener {
+//            // Success.
+//
+//        }
+}
