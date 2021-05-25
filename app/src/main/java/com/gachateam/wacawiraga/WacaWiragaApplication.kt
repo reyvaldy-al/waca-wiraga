795fe71280bb527dd4962925412ef57cefede56b
daya <daya.aeist@gmail.com>
2021-05-25 12:56:54 +0700

    plant timber logging

---
+++ app/src/main/java/com/gachateam/wacawiraga/WacaWiragaApplication.kt
@@ -0,0 +1,16 @@
+package com.gachateam.wacawiraga
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+
+class WacaWiragaApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
